776fd9c2362ad24d927d478cfe59a566c8d499c2
AnnaK87 <anna.kaukh@gmail.com>
2018-12-08 22:51:24 +0400

    Initial Commit

---
+++ Smak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smak
+//
+//  Created by Anna Kaukh on 12/8/18.
+//  Copyright © 2018 Anna Kaukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
